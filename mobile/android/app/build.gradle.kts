b6d2308ec0986729fa9084b43dbd5c37a7501c8e
wira <wirashaumaa@gmail.com>
2026-05-09 14:49:02 +0700

    feat: initialize project architecture with neo-brutalist design system and core feature modules

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -7,10 +7,11 @@ plugins {
 
 android {
     namespace = "com.example.rekber"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -25,9 +26,10 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -37,6 +39,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
