4e933852fef9a38851a84ab3cdec361b2649b1a2
wira <wirashaumaa@gmail.com>
2026-05-09 14:49:29 +0700

    feat: implement core feature modules including escrow state machine, brutalist UI components, and navigation structure

--- mobile/lib/core/usecases/usecase.dart
+++ mobile/lib/core/usecases/usecase.dart
@@ -4,8 +4,8 @@ import '../errors/failures.dart';
 
 /// Base UseCase contract for Clean Architecture
 /// [Type] = return type, [Params] = input parameters
-abstract class UseCase<Type, Params> {
-  Future<Either<Failure, Type>> call(Params params);
+abstract class UseCase<T, Params> {
+  Future<Either<Failure, T>> call(Params params);
 }
 
 /// For use cases that don't require parameters
